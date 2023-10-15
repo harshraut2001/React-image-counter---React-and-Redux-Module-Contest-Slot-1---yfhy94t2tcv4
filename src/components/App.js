8c6a5d65a9c1cdc4193f77e93fb4efee896b3083
hars21raut <hraut9069@gmail.com>
2023-10-15 11:38:21 +0530

    contest que

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,16 @@ import React, { useState } from 'react'
 import '../styles/App.css';
 import star from '../star.png'
 const App = () => {
+  const [dimensions,setDimenstions]=useState({width:300,height:300});
+  const increaseSize=()=>{
+    setDimenstions({
+      width:dimensions.width+2,
+      height:dimensions.height+2,
+    });
+  };
   return (
     <div id="main">
-      <img src={star}  height="300px" width="300px" />
+      <img src={star}  height={dimensions.height} width={dimensions.width} onClick={increaseSize} />
     </div>
   )
 }
